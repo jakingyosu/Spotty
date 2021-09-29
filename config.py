a359260e01839570c09e39c2a19394e18bf2252b
jakingy <jakingyosu@gmail.com>
2021-09-30 08:47:41 +1000

    proof of concept

---
+++ config.py
@@ -0,0 +1,11 @@
+# set of Discord ID's of users who can use dev commands on the bot
+OWNERS = {519472950928211978}
+
+# Default prefix the bot uses when it joins a server
+DEFAULT_PREFIX = "!"
+
+#token of the discord bot account
+TOKEN = "token"
+
+# the file where logs are kept
+LOG_FILE = "./log.txt" 
